f397c4632cf31ad2d1dcba23617a8a8c675e32d6
Anna777o <98017441+Anna777o@users.noreply.github.com>
2023-07-24 17:28:58 +0300

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -3,126 +3,40 @@
 #include <cpprest/json.h>
 #include <map>
 #include <algorithm>
+#include <iostream>
+#include "sdk.h"
+#include "sdk2.h"
+#include <vector>
 using namespace utility;
 using namespace web::http;
 using namespace web::http::client;
 using namespace web::json;
 
-class GLSystemSDK {
-private:
-	http_client client;
-
-public:
-	GLSystemSDK() : client(U("https://back.glsystem.net/api/v1/auth/login/")) {}
-	std::string access_token;
-	pplx::task<value> login(const std::string& username, const std::string& password) {
-		value payload;
-		payload[U("username")] = value::string(utility::conversions::to_string_t(username));
-		payload[U("password")] = value::string(utility::conversions::to_string_t(password));
-
-		http_request request(methods::POST);
-		request.headers().add(U("Content-Type"), U("application/json"));
-		request.set_body(payload);
-
-		return client.request(request)
-			.then([](http_response response) {
-			if (response.status_code() == status_codes::OK) {
-				return response.extract_json();
-			}
-			else {
-				throw std::runtime_error("Error: " + std::to_string(response.status_code()));
-			}
-		});
-	}
-	
-	
-
-};
-class GLSystemSDK_1 {
-private:
-	http_client client;
-
-public:
-	GLSystemSDK_1() : client(U("https://back.glsystem.net/api/v1/calculation")) {}
-	pplx::task<value> create(const std::string token, int project_id = 1, std::map<std::string, value> input_data = {}, std::string status="",
-		std::string callback_url="", bool external_api=false) {
-		value payload;
-		value dictObject;
-		for (const auto& pair : input_data) {
-			dictObject[utility::conversions::to_string_t(pair.first)] = pair.second;
-		}
-		payload[U("project_id")] = value::number((project_id));
-		payload[U("input_data")] = ((dictObject));
-		payload[U("status")] = value::string(utility::conversions::to_string_t(status));
-		payload[U("callback_url")] = value::string(utility::conversions::to_string_t(callback_url));
-		payload[U("external_api")] = value::boolean((external_api));
-		http_request request(methods::POST);
-		request.headers().add(U("Authorization: Bearer "),(token));
-		
-		request.set_body(payload);
-
-		return client.request(request)
-			.then([](http_response response) {
-			if (response.status_code() == status_codes::OK) {
-				return response.extract_json();
-			}
-			else {
-				throw std::runtime_error("Error: " + std::to_string(response.status_code()));
-			}
-		});
-
-	}
-	pplx::task<value> get(const std::string token, bool favorite=false, bool is_history=false, bool is_recalculate=false, std::string ordering="", int page=1,
-		int page_size=1, int project_id=0, std::string status="") {
-		value payload;
-		payload[U("favorite")] = value::boolean((favorite));
-		payload[U("is_history")] = value::boolean((is_history));
-		payload[U("is_recalculate")] = value::boolean((is_recalculate));
-		payload[U("ordering")] = value::string(utility::conversions::to_string_t(ordering));
-		payload[U("page")] = value::number((page));
-		payload[U("page_size")] = value::number((page_size));
-		payload[U("project_id")] = value::number((project_id));
-		payload[U("status")] = value::string(utility::conversions::to_string_t(status));
-		http_request request(methods::GET);
-		request.headers().add(U("Authorization: Bearer "), (token));
-		request.set_body(payload);
-
-		return client.request(request)
-			.then([](http_response response) {
-			if (response.status_code() == status_codes::OK) {
-				return response.extract_json();
-			}
-			else {
-				throw std::runtime_error("Error: " + std::to_string(response.status_code()));
-			}
-		});
-
-	}
-};
 int main() {
-	GLSystemSDK sdk;
-	GLSystemSDK_1 sdk1;
-	std::map<std::string, value> mp;
-	mp.insert({ "data",42});
-	sdk.login("test", "test") //use your own login and password
-		.then([](value json) {
-		int j = json.serialize().size();
-		std::cout << json.serialize().size() <<std:: endl;
-		for(int i=0;i<j;++i)
-			std::cout << json.serialize()[i];
+	sdk sdk;
+	sdk2 sdk1;
+	std::wstring token;
+	sdk.login("ovdanna@mail.ru", "Rjkj,jr02")
+		.then([&token](value json) {
+		std::wstring js= json.serialize();		
+		for (int i = 17; i < js.size()-1; ++i)
+		{
+			if (js[i + 1] != wchar_t(',')) {
+				token = token + js[i];
+				
+			}
+			else break;
+		}
+		std::wcout << token << std::endl;
+		
 	})
 		.wait();
-	//токен c3a4d4cdf8b8258e7c196fc0019943c7fc63b532	
-	sdk1.create("324324343", 1, {}, "new", "no", true)
-			.then([](value json) {
-			
-		})
-			.wait();
-	sdk1.get("324324343",false,false,false,"fist",1,1,1,"new")
-			.then([](value json) {
-			
-		})
-			.wait();
+
+	sdk1.get_by_id(token, 6514)
+		.then([](value json) {
+
+	})
+		.wait();
 
 	return 0;
 }
