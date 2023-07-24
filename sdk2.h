81c9591a5d90b1fcc7e2b574d431d0d421fd8f53
Anna777o <98017441+Anna777o@users.noreply.github.com>
2023-07-24 21:22:34 +0300

    Create sdk2.h

---
+++ sdk2.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+#include <cpprest/json.h>
+#include <map>
+#include <algorithm>
+#include <iostream>
+using namespace utility;
+using namespace web::http;
+using namespace web::http::client;
+using namespace web::json;
+class sdk2
+{
+private:
+	
+	std::string m_apiUrl;
+	std::string m_id;
+	std::wstring m_token;
+
+public:
+	
+	sdk2(const std::string& apiUrl, const std::string& id, const std::wstring& token)
+		: m_apiUrl(apiUrl), m_id(id), m_token(token)
+	{
+	}
+	pplx::task<value> get_by_id(std::wstring & token, int id);
+};
+
