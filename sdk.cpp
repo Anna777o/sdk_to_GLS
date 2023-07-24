81349baa189db9db1d5985157f1f0fac72fd4f84
Anna777o <98017441+Anna777o@users.noreply.github.com>
2023-07-24 17:27:59 +0300

    Update sdk.cpp

--- sdk.cpp
+++ sdk.cpp
@@ -1,24 +1,24 @@
 #include "sdk.h"
 
-	pplx::task<value> sdk::login(const std::string & username, const std::string & password)
-	{
-		value payload;
+pplx::task<value> sdk::login(const std::string & username, const std::string & password)
+{
+	value payload;
 
-		payload[U("username")] = value::string(utility::conversions::to_string_t(username));
-		payload[U("password")] = value::string(utility::conversions::to_string_t(password));
+	payload[U("username")] = value::string(utility::conversions::to_string_t(username));
+	payload[U("password")] = value::string(utility::conversions::to_string_t(password));
 
-		http_request request(methods::POST);
-		request.headers().add(U("Content-Type"), U("application/json"));
-		request.set_body(payload);
+	http_request request(methods::POST);
+	request.headers().add(U("Content-Type"), U("application/json"));
+	request.set_body(payload);
 
-		return client.request(request)
-			.then([](http_response response) {
-			if (response.status_code() == status_codes::OK) {
+	return client.request(request)
+		.then([](http_response response) {
+		if (response.status_code() == status_codes::OK) {
 
-				return response.extract_json();
-			}
-			else {
-				throw std::runtime_error("Error: " + std::to_string(response.status_code()));
-			}
-		});
-	}
+			return response.extract_json();
+		}
+		else {
+			throw std::runtime_error("Error: " + std::to_string(response.status_code()));
+		}
+	});
+}
