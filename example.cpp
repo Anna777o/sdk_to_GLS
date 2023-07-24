5885a7a2d60c04c68ae59bcccbd26ba890577c52
Anna777o <98017441+Anna777o@users.noreply.github.com>
2023-07-24 21:24:57 +0300

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,9 +1,3 @@
-#include <cpprest/http_client.h>
-#include <cpprest/filestream.h>
-#include <cpprest/json.h>
-#include <map>
-#include <algorithm>
-#include <iostream>
 #include "sdk.h"
 #include "sdk2.h"
 #include <vector>
@@ -11,11 +5,12 @@ using namespace utility;
 using namespace web::http;
 using namespace web::http::client;
 using namespace web::json;
-//проверка работы авторизации
+//Пример на авторизацию и получение информации о расчете по его id
 int main() {
 	sdk sdk;
+	
 	std::wstring token;
-	sdk.login("test", "test")
+	sdk.login("ovdanna@mail.ru", "test")
 		.then([&token](value json) {
 		std::wstring js= json.serialize();		
 		for (int i = 17; i < js.size()-1; ++i)
@@ -26,10 +21,19 @@ int main() {
 			}
 			else break;
 		}
+		//std::wcout << js << std::endl;
 		std::wcout << token << std::endl;
 		
-	})
-		.wait();
+	}).wait();
+	std::string url1 = "https://back.glsystem.net/api/v1/calculation/";
+	sdk2 sdk1(url1,"30989",token);
+	sdk1.get_by_id(token, 30989)
+		.then([](value json) {
+		std::wstring js = json.serialize();
+		std::wcout << js << std::endl;
+
+	}).wait();
+	
 
 	return 0;
 }
